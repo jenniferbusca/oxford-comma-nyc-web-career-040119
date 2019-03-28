b3cee5302e37dbad6c27089a0c4b73607b37d1bd
Jennifer Busca <jennifer.busca@gmail.com>
2019-03-28 19:57:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,9 +7,9 @@ def oxford_comma(array)
     string = array.join(" and ")
   elsif array.length == 3
     new_arr << array[0] + ","
-    new_arr << array[1] + ", and "
+    new_arr << array[1] + ", and"
     new_arr << array[-1]
-    new_arr.join
+    new_arr.join(" ")
   elsif array.length > 3
     string = array.join(", ")
   end
